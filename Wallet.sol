40db1ef229361e11b02f278b76efe97a9391ac0f
Remix Workspace <>
2022-10-13 18:50:58 -0700

    Commit

--- Wallet.sol
+++ Wallet.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 //Multi-Sig-Wallet
 //(Done)Anyone can deposit into smart contract(wallet). We need a mapping to track accnt balances.
 // Contract creator should be able to input //1: address of Owner,// 2. number of approvals req for transfer in "constructor". Ex set input to 3 addresses, set approval limit to 3.
@@ -7,21 +8,27 @@
 //Owners(1-3) Should be able to approve Txfer request.
 //When a Txfer request has the required approvals, txfer should be sent.
 pragma solidity 0.7.5;
+pragma abicoder v2;
 
 contract Wallet{
 
-    address = owner;
+    
     address rich = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
     address ben = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
 
     int[3] owners =[owner, rich, john];
 
-    constructor(address _owner, uint _approvals,){
-        owner = msg.sender
-        
+    constructor(address _owner, uint _approvals){
+        owner = msg.sender;
+        uint limit = 2; //Max number of signatures
+
     }
 
     mapping(address => uint) balance;//Tie balances to addresses
+
+    //DoubleMapping 
+    mapping(address => mapping(uint =>bool)) approvals;
+
 
     //deposit just makes a number its not really ether.
     function deposit( ) public payable returns(uint){
@@ -29,11 +36,31 @@ contract Wallet{
         return balance[msg.sender];//deposit fxn returns uint
     }
 
-    //Use array that holds all 3 addresses?
-    function transfer(address recipient, uint amount) public {
-        //require(array= 2)
+    function Approved(uint tid, bool appr) public returns(uint){
+        // boolean answer to mapping. transferid= tid
+        approvals[msg.sender][tid] = appr;
     }
 
+    //Define what properties a Transfer should have no actual values.
+    struct Transfer{
+        address from;
+        address to;
+        uint amount;
+        bool approval;
+    }
+
+    Transfer[] transferRequests;
+
+    //Use array that holds all 3 addresses?
+    function addTransfer(address recipient, uint amount, bool approval) public {
+        //First save request.
+        Transfer[] newTransfer = Transfer(msg.sender, recipient, amount);
+        transferRequests.push(); //
+    }
+
+    //To get struct Test to return must enter pragma abicoder v2, ln 11;
+    function getTransfer(uint _index) public view returns (Test memory)
+      return transferRequests[_index]
     //with mappings allows us to track a balance. How does ether tie in?
     function getBalance() public view returns(uint){
       return  balance[msg.sender]; //msg.sender will always be my own address.
